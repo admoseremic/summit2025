2ac8135803888ecaf27da09632ba73284072c059
Russ Whitchurch <rwhitchu@adobe.com>
2024-12-13 17:37:44 -0700

    Replace Donkey Kong with Runner

--- public/donkeykong.js
+++
@@ -1,9 +0,0 @@
-function startDonkeyKong() {
-    const gameContent = document.getElementById('gameContent');
-    gameContent.innerHTML = '';
-    const square = document.createElement('div');
-    square.style.width = '100px';
-    square.style.height = '100px';
-    square.style.backgroundColor = 'brown';
-    gameContent.appendChild(square);
-}
